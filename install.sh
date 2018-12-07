0cbaab8a2411cbd559819ee2d8027deab9b7f25f
pawneeranger <33731788+pawneeranger@users.noreply.github.com>
2018-12-07 01:46:04 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,9 @@ mkdir quarantaine
 mkdir sains
 chmod +x ./launch.sh
 printf "\n\n\033[0;32mINSTALATION TERMINEE\033[0m\n"
+printf "\n...Téléchargement de la base de données Yara..."
+./launch.sh -u
+printf "\n\n\033[0;32mTéléchargement terminé\033[0m\n"
 printf "Pour lancer l'outil: "
-printf "\n./launch.sh (depuis ce répertoire)\n"
-cd ..
+printf "\n\t./launch.sh (depuis ce répertoire)\n"
 rm -rf install.sh

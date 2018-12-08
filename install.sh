d006bce4615a3aa9742960dc25056de80480eefa
pawneeranger <33731788+pawneeranger@users.noreply.github.com>
2018-12-08 02:24:22 +0100

    v2
    Install with alias, absolute path

--- install.sh
+++ install.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install yara inotify-tools
+sudo apt install yara inotify-tools clamav clamav-freshclam python3
+sudo freshclam
+sudo cp ./mini_projet_yara /usr/bin/mini_projet_yara
+sudo chmod +x /usr/bin/mini_projet_yara
+cd /var
+sudo mkdir mini_projet_yara
+sudo chown $USER ./mini_projet_yara/
+cd mini_projet_yara
 mkdir yara_db
 cd yara_db
+git clone https://github.com/Augustin-FL/clamav_to_yara3.git
+git clone https://github.com/Yara-Rules/rules.git
 mkdir custom
 cd ..
 mkdir depot
 mkdir quarantaine
 mkdir sains
-chmod +x ./launch.sh
-printf "\n\n\033[0;32mINSTALATION TERMINEE\033[0m\n"
-printf "\n...Téléchargement de la base de données Yara..."
-./launch.sh -u
-printf "\n\n\033[0;32mTéléchargement terminé\033[0m\n"
-printf "Pour lancer l'outil: "
-printf "\n\t./launch.sh (depuis ce répertoire)\n"
-rm -rf install.sh
+echo "alias mini_projet_yara='bash /usr/bin/mini_projet_yara'" >> ~/.bashrc
+. ~/.bashrc
+echo "Installation is complete"
+mini_projet_yara -h
